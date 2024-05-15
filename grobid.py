8a5881fe0aead745e24624959e7476bcf11a66b1
uhrishabh <uhrishabh@gmail.com>
2024-05-15 09:05:40 +0100

    Fixing Repo

--- grobid.py
+++
@@ -1,2 +0,0 @@
-import scipdf
-article_dict = scipdf.parse_pdf_to_dict('/Users/ricky/PycharmProjects/OHE_OCR/committee_papers/committee-papers-2_ta380.pdf', as_list=False)
--- mlx_llm.py
+++ mlx_llm.py
@@ -1,35 +1,7 @@
 from mlx_lm import load, generate
 
-model, tokenizer = load("mlx-community/quantized-gemma-2b")
+model, tokenizer = load("mlx-community/Hermes-2-Pro-Mistral-7B-8bit")
 MEDICAL_DOCUMENT = 'Fluocinolone acetonide intravitreal implant is recommended, within its marketing authorisation, as an option for treating visual impairment caused by chronic diabetic macular oedema that has not responded well enough to available treatments in adults. It is recommended only if the company provides it according to thecommercial arrangement.'
 
-response = generate(model, tokenizer, prompt=f"what is recommended:{MEDICAL_DOCUMENT} \n Generate response from above context only", verbose=True)
+response = generate(model, tokenizer, prompt=f"Context:{MEDICAL_DOCUMENT} \n generate a answer only from the context Question: which medicine is recommended? answer: ", verbose=True,repetition_context_size=0)
 
-
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-checkpoint = "bigscience/bloomz-560m"
-
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto")
-prompt=f"what is recommended:{MEDICAL_DOCUMENT} \n Generate response from above context only"
-
-inputs = tokenizer.encode(prompt, return_tensors="pt")
-outputs = model.generate(inputs)
-print(tokenizer.decode(outputs[0]))
-
-
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-checkpoint = "CohereForAI/aya-101"
-
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-aya_model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
-
-# Turkish to English translation
-prompt=f"what is recommended:{MEDICAL_DOCUMENT} \n Generate response from above context only"
-
-tur_inputs = tokenizer.encode(prompt, return_tensors="pt")
-tur_outputs = aya_model.generate(tur_inputs, max_new_tokens=128)
-print(tokenizer.decode(tur_outputs[0]))
